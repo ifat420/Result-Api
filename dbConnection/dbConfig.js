97b966ffc5aeef3122ff603c4f9f63efc58fc948
swadhIn <swadhin.just@gmail.com>
2018-08-08 11:06:55 +0600

    dbconnection config

--- dbConnection/dbConfig.js
+++ dbConnection/dbConfig.js
@@ -1,14 +1,14 @@
 module.exports = {
-    user          : process.env.NODE_ORACLEDB_USER || "Addmission",
+    user          : process.env.NODE_ORACLEDB_USER || "sdn",
   
     // Instead of hard coding the password, consider prompting for it,
     // passing it in an environment variable via process.env, or using
     // External Authentication.
-    password      : process.env.NODE_ORACLEDB_PASSWORD || "ifat",
+    password      : process.env.NODE_ORACLEDB_PASSWORD || "A1111111a",
   
     // For information on connection strings see:
  
-    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/orcl",
+    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost:1523/orcl",
   
     // Setting externalAuth is optional.  It defaults to false.  See:
     // https://oracle.github.io/node-oracledb/doc/api.html#extauth
